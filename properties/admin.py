b616ef97f3a6336e47eab33f28573cf0fee342d9
Sanctusz <yohans89@gmail.com>
2019-05-06 13:20:11 +0000

    admin

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Properties
+from .models import PropertyDetails
+from .models import PropertiesImage
+from .models import PropertyAddress
+
+
+admin.site.register(Properties)
+admin.site.register(PropertyDetails)
+admin.site.register(PropertyAddress)
+admin.site.register(PropertiesImage)
