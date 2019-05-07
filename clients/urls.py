aefd1b721ee2d398966eef083545916cbb718f0b
Sanctusz <yohans89@gmail.com>
2019-05-07 14:12:33 +0000

    authentication for user

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name="clients-index"),
-
+    path('register/', views.register, name="clients-register"),
+    path('login/', LoginView.as_view(template_name="clients/login.html"), name="clients-login"),
+    path('logout/', LogoutView.as_view(next_page="clients-login"), name="clients-logout")
 ]
