36cd55de936727f59ae0269962edfeae0fdf74ed
Sanctusz <yohans89@gmail.com>
2019-05-11 18:03:40 +0000

    purchase getting there

--- purchases/forms/purchases_forms.py
+++ purchases/forms/purchases_forms.py
@@ -2,14 +2,49 @@ from django.forms import ModelForm, widgets
 from django import forms
 from purchases.models import *
 from clients.models import Profile
-from clients.forms.profile_form import ProfileForm
 
 class CreditCardForm(ModelForm):
     class Meta:
+        labels = {
+            'ccName': 'Name on the Credit Card',
+            'ccNumber': 'Credit Card Number',
+            'CVC': 'CVC',
+            'expirationDate': 'Expiration Date'
+        }
         model = CreditCard
-        exclude = ['id', 'expirationDate']
+        exclude = ['id']
         widgets = {
             'ccName': widgets.TextInput(attrs={'class': 'form-control'}),
             'ccNumber': widgets.TextInput(attrs={'class': 'form-control', 'maxlength': 16, 'pattern': '[0-9]{16}'}),
-            'CVC': widgets.TextInput(attrs={'class': 'form-control', 'maxlength': 3, 'pattern': '[0-9]{3}'})
+            'CVC': widgets.TextInput(attrs={'class': 'form-control', 'maxlength': 3, 'pattern': '[0-9]{3}'}),
+            'expirationDate': widgets.TextInput(attrs={'class': 'form-control', 'maxlength': 5, 'pattern': '[0-9]{5}'})
         }
+
+class ProfileForm(ModelForm):
+    class Meta:
+        labels = {
+            'fname': 'First Name',
+            'mname': 'Middle Name',
+            'lname': 'Last Name',
+            'streetName': 'Street Name',
+            'houseNumber': 'House Number',
+            'zipCode': 'Zip Code'
+        }
+        model = Profile
+        exclude = ['id', 'image']
+        widgets = {
+            'fname': widgets.TextInput(attrs={'class': 'form-control'}),
+            'mname': widgets.TextInput(attrs={'class': 'form-control'}),
+            'lname': widgets.TextInput(attrs={'class': 'form-control'}),
+            'SSN': widgets.TextInput(attrs={'class': 'form-control', 'maxlength': 10, 'pattern': '[0-9]{10}'}),
+            'streetName': widgets.TextInput(attrs={'class': 'form-control'}),
+            'houseNumber': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'zipCode': widgets.TextInput(attrs={'class': 'form-control'}),
+            'city': widgets.TextInput(attrs={'class': 'form-control'}),
+            'country': widgets.TextInput(attrs={'class': 'form-control'})
+        }
+
+class PurchaseForm(ModelForm):
+    class Meta:
+        model = Purchases
+        exclude = ['id', 'buyer', 'realtyAgent', 'property', 'payment', 'date']
--- purchases/models.py
+++ purchases/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from properties.models import *
 from agents.models import *
 from clients.models import *
+from django.utils.timezone import now
 
 
 class CreditCard(models.Model):
     ccName = models.CharField(max_length=255)
     ccNumber = models.CharField(max_length=16)
     CVC = models.CharField(max_length=3)
-    expirationDate = models.DateField()
+    expirationDate = models.CharField(max_length=5)
 
 
 class Purchases(models.Model):
@@ -16,4 +17,4 @@ class Purchases(models.Model):
     realtyAgent = models.ForeignKey(Agents, on_delete=models.CASCADE)
     property = models.ForeignKey(Properties, on_delete=models.CASCADE)
     payment = models.ForeignKey(CreditCard, on_delete=models.CASCADE)
-    date = models.DateTimeField(unique=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
