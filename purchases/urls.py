a06162ee3348dde1c774ea9de8077dfe2c447647
gozdesigurdsson <gozdesigurdsson@gmail.com>
2019-05-14 13:09:16 +0000

    for gozde branch

--- purchases/urls.py
+++ purchases/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>', views.purchase_property, name='purchase')
+    # path('<int:id>', views.purchase_property, name='purchase'),
+    path('<int:id>/contact_info', views.get_contact_info, name='contact_info'),
+    path('<int:id>/card_info', views.get_card_info, name='card_info'),
+    path('<int:id>/confirm_purchase', views.confirm_purchase, name='confirm_purchase')
 ]
--- purchases/views.py
+++ purchases/views.py
@@ -3,43 +3,113 @@ from django.contrib.auth.decorators import login_required
 from purchases.forms.purchases_forms import *
 
 
+
 @login_required()
-def purchase_property(request, id):
+def get_contact_info(request, id):
     profile = Profile.objects.filter(user=request.user).first()
     if request.method == 'POST':
         property_obj = get_object_or_404(Properties, pk=id)
-        agent_obj = get_object_or_404(Agents, pk=property_obj.agent.id)
-
         cinfoForm = ProfileForm(instance=profile, data=request.POST)
         profile = cinfoForm.save(commit=False)
         profile.user = request.user
 
-        ccForm = CreditCardForm(data=request.POST)
-        purchaseForm = PurchaseForm(data=request.POST)
-
-        if cinfoForm.is_valid() and ccForm.is_valid():
+        if cinfoForm.is_valid():
             profile.save()
             cinfo = cinfoForm.save()
-            ccard = ccForm.save()
-            purchase = purchaseForm.save(commit=False)
-            purchase.buyer = cinfo
-            purchase.realtyAgent = agent_obj
-            purchase.property = property_obj
-            purchase.payment = ccard
-            if purchaseForm.is_valid():
-                purchaseForm.save()
-                propForm = PropertyForm(data=request.POST, instance=property_obj)
-                statuschange = propForm.save(commit=False)
-                statuschange.status = 'sold'
-                statuschange.save()
-                return redirect('index')
+            return redirect("card_info", property_obj.id,)
+
     context = {
         'property': get_object_or_404(Properties, pk=id),
         'profileForm': ProfileForm(instance=profile),
+    }
+    return render(request, 'purchases/contact_info.html', context)
+
+
+@login_required()
+def get_card_info(request, id, cinfo):
+    profile = Profile.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        property_obj = get_object_or_404(Properties, pk=id)
+        ccForm = CreditCardForm(data=request.POST)
+        profile.user = request.user
+        if ccForm.is_valid():
+            ccForm.save()
+            return redirect("confirm_purchase", property_obj.id)
+
+    context = {
+        'property': get_object_or_404(Properties, pk=id),
         'creditCardForm': CreditCardForm(),
+    }
+    return render(request, 'purchases/card_info.html', context)
+
+
+@login_required()
+def confirm_purchase(request, id):
+    profile = Profile.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        property_obj = get_object_or_404(Properties, pk=id)
+        agent_obj = get_object_or_404(Agents, pk=property_obj.agent.id)
+        purchaseForm = PurchaseForm(data=request.POST)
+        profile.user = request.user
+
+        purchase = purchaseForm.save(commit=False)
+        purchase.buyer = cinfo
+        purchase.realtyAgent = agent_obj
+        purchase.property = property_obj
+        purchase.payment = ccard
+
+        if purchaseForm.is_valid():
+            purchaseForm.save()
+            propForm = PropertyForm(data=request.POST, instance=property_obj)
+            statuschange = propForm.save(commit=False)
+            statuschange.status = 'sold'
+            statuschange.save()
+            return redirect('index')
+    context = {
+        'property': get_object_or_404(Properties, pk=id),
         'purchaseForm': PurchaseForm()
     }
-    return render(request, 'purchases/purchase.html', context)
+    return render(request, 'purchases/confirm_purchase.html', context)
+
+
+#
+# @login_required()
+# def purchase_property(request, id):
+#     profile = Profile.objects.filter(user=request.user).first()
+#     if request.method == 'POST':
+#         property_obj = get_object_or_404(Properties, pk=id)
+#         agent_obj = get_object_or_404(Agents, pk=property_obj.agent.id)
+#
+#         cinfoForm = ProfileForm(instance=profile, data=request.POST)
+#         profile = cinfoForm.save(commit=False)
+#         profile.user = request.user
+#
+#         ccForm = CreditCardForm(data=request.POST)
+#         purchaseForm = PurchaseForm(data=request.POST)
+#
+#         if cinfoForm.is_valid() and ccForm.is_valid():
+#             profile.save()
+#             cinfo = cinfoForm.save()
+#             ccard = ccForm.save()
+#             purchase = purchaseForm.save(commit=False)
+#             purchase.buyer = cinfo
+#             purchase.realtyAgent = agent_obj
+#             purchase.property = property_obj
+#             purchase.payment = ccard
+#             if purchaseForm.is_valid():
+#                 purchaseForm.save()
+#                 propForm = PropertyForm(data=request.POST, instance=property_obj)
+#                 statuschange = propForm.save(commit=False)
+#                 statuschange.status = 'sold'
+#                 statuschange.save()
+#                 return redirect('index')
+#     context = {
+#         'property': get_object_or_404(Properties, pk=id),
+#         'profileForm': ProfileForm(instance=profile),
+#         'creditCardForm': CreditCardForm(),
+#         'purchaseForm': PurchaseForm()
+#     }
+#     return render(request, 'purchases/purchase.html', context)
 
 
 '''
@@ -64,3 +134,4 @@ def purchase_property(request, id):
             prop.status = "sold"
             prop.save()
 '''
+
