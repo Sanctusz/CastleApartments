8a3f9d1a07d27a648227a50f5482a3e4be63e365
Sanctusz <yohans89@gmail.com>
2019-05-15 17:07:07 +0000

    Update settings.py

--- CastleApartments/settings.py
+++ CastleApartments/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'CastleApartments.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bzsiedpb',
-        'USER': 'bzsiedpb',
-        'PASSWORD': 'bVQrfVrq6CvbZwZLLF56dbCyySmpLLFw',
+        'NAME': 'lgoyyppp',
+        'USER': 'lgoyyppp',
+        'PASSWORD': 'CkcPVyTLVPIp4zYWpI1pc6X1M-nJ19w2',
         'HOST': 'manny.db.elephantsql.com',
         'PORT': '5432'
     }
