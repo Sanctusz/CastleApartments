bcca0ad80c17474a2a418b4dd15ee783e0afde7e
Sanctusz <yohans89@gmail.com>
2019-05-16 19:47:03 +0000

    Remove Images

--- properties/urls.py
+++ properties/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create_property', views.create_property, name='create-property'),
     path('update_property/<int:id>', views.update_property, name='update-property'),
     path('email/', views.emailView, name='email'),
-    path('add_images/<int:id>', views.add_images, name='add-images')
+    path('add_images/<int:id>', views.add_images, name='add-images'),
+    path('remove_image/<int:prop_id>/<int:image_id>', views.remove_image, name='remove-image')
 ]
