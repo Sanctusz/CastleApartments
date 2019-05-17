014fe77c72c8b2d8aeb924164a525f59b975f502
Sanctusz <yohans89@gmail.com>
2019-05-17 14:45:59 +0000

    Nýjasta nýjasta
    Confirmation Steps in Purchases ready

--- static/js/purchases.js
+++ static/js/purchases.js
@@ -1,23 +1,32 @@
 $(document).ready(function () {
+    var ci_done = false;
+    var cc_done = false;
+
     $('#ci_a').on('click', function (e) {
         e.preventDefault();
-        show('ci_step','cc_step','confirm_step');
+        show('ci_step', 'cc_step', 'confirm_step');
     });
 
     $('#cc_a').on('click', function (e) {
         e.preventDefault();
-        show('cc_step','ci_step','confirm_step');
+        if (ci_done === true) {
+            show('cc_step', 'ci_step', 'confirm_step');
+        }
     });
 
     $('#confirm_a').on('click', function (e) {
         e.preventDefault();
-        show('confirm_step','ci_step','cc_step');
+        if (cc_done === true) {
+            show('confirm_step', 'ci_step', 'cc_step');
+        }
     });
 
     $('#cc_next').on('click', function (e) {
         e.preventDefault();
-        show('cc_step','ci_step','confirm_step', 'cc_li', 'cc_a');
-
+        if (ci_filled()) {
+            show('cc_step', 'ci_step', 'confirm_step', 'cc_li', 'cc_a');
+            ci_done = true;
+        }
     });
 
     $('#confirm_next').on('click', function (e) {
@@ -35,7 +44,7 @@ $(document).ready(function () {
         var ccnumber = $('#id_ccNumber').val();
         var cvc = $('#id_CVC').val();
         var expirationdate = $('#id_expirationDate').val();
-        show('confirm_step','cc_step','ci_step','confirm_li','confirm_a');
+        show('confirm_step', 'cc_step', 'ci_step', 'confirm_li', 'confirm_a');
         document.getElementById("fname").innerHTML = fname;
         document.getElementById("mname").innerHTML = mname;
         document.getElementById("lname").innerHTML = lname;
@@ -49,6 +58,7 @@ $(document).ready(function () {
         document.getElementById("ccNumber").innerHTML = ccnumber;
         document.getElementById("CVC").innerHTML = cvc;
         document.getElementById("expirationDate").innerHTML = expirationdate;
+        cc_done = true;
     });
 });
 
@@ -61,11 +71,30 @@ function show(shown, hidden, hidden2, completed_li, completed_a) {
     }
     if (completed_a === 'cc_a') {
         var anchor = document.getElementById(completed_a);
-        anchor.setAttribute('href','#');
+        anchor.setAttribute('href', '#');
     }
     if (completed_a === 'confirm_a') {
         var anchor = document.getElementById(completed_a);
-        anchor.setAttribute('href','#');
+        anchor.setAttribute('href', '#');
     }
     return false;
+}
+
+function ci_filled() {
+    var fname = $('#id_fname').val();
+    var mname = $('#id_mname').val();
+    var lname = $('#id_lname').val();
+    var ssn = $('#id_ssn').val();
+    var streetname = $('#id_streetName').val();
+    var housenumber = $('#id_houseNumber').val();
+    var zipcode = $('#id_zipCode').val();
+    var city = $('#id_city').val();
+    var country = $('#id_country').val();
+    if (fname === '' || mname === '' || lname === '' || ssn === '' || streetname === ''
+        || housenumber === '' || zipcode === '' || city === '' || country === '') {
+        return false
+    }
+    else {
+        return true
+    }
 }
